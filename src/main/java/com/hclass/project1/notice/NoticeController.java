ddace5db162f67a963d492ab7f32d61945d356e2
castlebean100 <71800772+castlebean100@users.noreply.github.com>
2020-11-13 13:29:54 +0900

    notice_2

--- src/main/java/com/hclass/project1/notice/NoticeController.java
+++ src/main/java/com/hclass/project1/notice/NoticeController.java
@@ -1,22 +1,7 @@
 package com.hclass.project1.notice;
 
-<<<<<<< Updated upstream
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/notice/**")
-public class NoticeController {
-	
-
-	@RequestMapping("/notice/noticeList")
-	public String noticeList() {
-		return "notice/noticeList";
-	}
-	
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 
 @Controller
 @RequestMapping ("/notice/**")
@@ -27,5 +12,4 @@ public class NoticeController {
 		return "notice/noticeList";
 	}
 
->>>>>>> Stashed changes
 }
