866f62a1b22a75fcd38b8765556b5637c8385429
jay1260 <jay1260@naver.com>
2020-11-20 16:32:11 +0900

    done

--- src/main/java/com/hclass/project1/Interceptor/PayMemberInterceptor.java
+++ src/main/java/com/hclass/project1/Interceptor/PayMemberInterceptor.java
@@ -2,44 +2,51 @@ package com.hclass.project1.Interceptor;
 
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.hclass.project1.member.MemberDTO;
+import com.hclass.project1.pay.PayDAO;
 import com.hclass.project1.pay.PayDTO;
 
 @Component
 public class PayMemberInterceptor extends HandlerInterceptorAdapter{
-
+	
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-
-		String method = request.getMethod();
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("PayMemberInterceptor");
 		
-		if(method.equals("POST")) {
-			return;
-		}
+		String id = request.getParameter("id");
+		
+		System.out.println("Pay" + id);
+		
+		PayDTO payDTO = new PayDTO();
+		payDTO.setId(id);
+		
 		
 		HttpSession session = request.getSession();
 		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
-		String memberId = memberDTO.getId();
 		
-		Map<String, Object> model = modelAndView.getModel();
-		PayDTO payDTO = (PayDTO)model.get("pay");
-		String paymentId = payDTO.getId();
 		
-		if(!memberId.equals(paymentId)) {
-			modelAndView.addObject("msg", "잘못된 경로 접속입니다..");
-			modelAndView.addObject("path", "../");
-			modelAndView.setViewName("common/result");
+		boolean check = false;
+		
+		if(payDTO.getId().equals(memberDTO.getId())) {
+			check = true;
 		}
-		
+		else {
+			request.setAttribute("msg", "권한이 없는 경로입니다.");
+			request.setAttribute("path", "../");
+			RequestDispatcher view = request.getRequestDispatcher("../WEB-INF/views/common/result.jsp");
+			view.forward(request, response);
+		}
+		return check;
 	}
 	
 }
