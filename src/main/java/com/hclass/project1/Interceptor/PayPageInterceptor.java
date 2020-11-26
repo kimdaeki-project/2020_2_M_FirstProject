46e3ca969eaef985675d3bed4a8a38a599a82b19
jay1260 <jay1260@naver.com>
2020-11-26 17:53:17 +0900

    done

---
+++ src/main/java/com/hclass/project1/Interceptor/PayPageInterceptor.java
@@ -0,0 +1,38 @@
+package com.hclass.project1.Interceptor;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.hclass.project1.member.MemberDTO;
+
+@Component
+public class PayPageInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
+		
+		boolean check = false;
+		if(memberDTO.getTrainer().equals("M")) {
+			check = true;
+		}else {
+			request.setAttribute("msg", "회원 전용 페이지입니다.");
+			request.setAttribute("path", "../");
+			
+			RequestDispatcher view = request.getRequestDispatcher("../WEB-INF/views/common/result.jsp");
+			view.forward(request, response);
+		}
+		
+		return check;
+	}
+	
+}
